973db208b508de1b221030d78213dce586109f77
Pranav Maniar <pranav9428@gmail.com>
2015-10-25 17:20:32 +0530

    Manual Process Scenario Initial layout is complete. Will work more on enhancing it..

---
+++ js/test.js
@@ -0,0 +1,14 @@
+//        var socket = io('http://172.27.0.140:3000/', {
+//            query: 'id=1234'
+//        });
+//
+//        socket.on('UP', function (msg) {
+//            //$('#messages').append($('<li>').text(msg));
+//            window.scrollBy(0, -100);
+//        });
+//
+//
+//        socket.on('DOWN', function (msg) {
+//            //$('#messages').append($('<li>').text(msg));
+//            window.scrollBy(0, 100);
+//        });
